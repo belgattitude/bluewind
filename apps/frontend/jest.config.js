2c9e5843109ec995ffc19cec52c37db08d555266
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-07-18 11:06:43 +0200

    jest configured

--- apps/frontend/jest.config.js
+++ apps/frontend/jest.config.js
@@ -1,26 +1,27 @@
 module.exports = {
     verbose: true,
     testURL: 'http://localhost/',
+    collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts', '!src/__tests__/*'],
+    setupFiles: ['./jest.stubs.ts'],
+    setupFilesAfterEnv: ['./jest.tests.ts'],
+    testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
     transform: {
-        '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
+        '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/ts-jest',
+        '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+        '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
     },
-    transformIgnorePatterns: ['<rootDir>/node_modules/(?!(lodash-es))'],
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'mjs', 'jsx', 'json', 'node'],
+    transformIgnorePatterns: [
+        '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
+        '^.+\\.module\\.(css|sass|scss)$',
+        '<rootDir>/node_modules/(?!(lodash-es))',
+    ],
+    modulePaths: [],
     moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-            '<rootDir>/__mocks__/fileMock.js',
-        '\\.(css|less|scss)$': 'identity-obj-proxy',
-        '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'identity-obj-proxy',
-        '@src/(.*)$': '<rootDir>/src/js/$1',
-        '@config/(.*)$': '<rootDir>/src/config/$1',
-        '@assets/(.*)$': '<rootDir>/src/assets/$1',
-        '@thirdparty/(.*)$': '<rootDir>/src/thirdparty/$1',
-        '@data/(.*)$': '<rootDir>/src/data/$1',
-        '@styles/*': '<rootDir>/src/styles/$1',
-        // fix lodash esm issues, by replacing lodash-es by commonjs one
+        '^react-native$': 'react-native-web',
+        '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
         '^lodash-es$': 'lodash',
     },
+    moduleFileExtensions: ['web.js', 'js', 'web.ts', 'ts', 'web.tsx', 'tsx', 'mjs', 'json', 'web.jsx', 'jsx', 'node'],
     globals: {
         window: {},
         'ts-jest': {
@@ -29,8 +30,4 @@ module.exports = {
             //babelConfig: '.babelrc',
         },
     },
-    //setupFiles: ['./jest.stubs.ts', 'jest-localstorage-mock'],
-    setupFiles: ['./jest.stubs.ts'],
-    setupFilesAfterEnv: ['./jest.tests.ts'],
-    collectCoverageFrom: ['src/**/*.+(ts|tsx|js|jsx)', '!src/__tests__/*'],
 };
