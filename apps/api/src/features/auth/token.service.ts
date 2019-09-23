308040a6ccbd5a1b35ce8c9573105f0807728e56
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-09-23 12:31:15 +0200

    Added token store and service

---
+++ apps/api/src/features/auth/token.service.ts
@@ -0,0 +1,71 @@
+import {Secret, sign, SignOptions, verify, VerifyOptions} from "jsonwebtoken";
+import {Result} from "../../core/result";
+
+type TokenPayload = {
+    [key: string]: number | string
+}
+
+
+type DefaultOptions = {
+    sign: SignOptions,
+    verify: VerifyOptions
+}
+
+export class TokenService {
+    private secret: string;
+    private defaultOptions: DefaultOptions;
+    constructor(secret: string, defaultOptions: DefaultOptions) {
+        this.secret = secret;
+        this.defaultOptions = defaultOptions;
+    }
+
+    /**
+     *
+     * @param payload
+     * @param expiresIn expressed in seconds or a string describing a time span [zeit/ms](https://github.com/zeit/ms.js)
+     * @param options
+     */
+    createToken(payload: TokenPayload, expiresIn: string|number, options: SignOptions={}): string {
+        return sign(payload, this.secret, {
+            ... this.defaultOptions.sign,
+            ... options,
+            ... {expiresIn: expiresIn}
+        });
+    }
+
+    verify<T extends {[key: string]: string|number}>(token: string, options: VerifyOptions={}): Result<T, Error> {
+        try {
+            console.log('cool', this.secret);
+            //return Result.fail('Cool error');
+            const verified = verify(token, this.secret, {
+                ...this.defaultOptions.verify,
+                ...options
+            });
+            if (typeof verified === 'string') {
+                return Result.fail(new Error(`Unexpected return type as string: ${verified}`))
+            }
+            return Result.ok(verified as unknown as T);
+        } catch (e) {
+            return Result.fail(new Error(`Error: ${e}`))
+        }
+    }
+
+    static createFormEnv(): TokenService {
+        // TODO create real env
+        const defaultOptions: DefaultOptions = {
+            sign: {
+                algorithm: 'HS256',
+                expiresIn: 60
+            },
+            verify: {
+                algorithms: ['HS256'],
+            }
+        }
+
+        return new TokenService(
+            'mySuperSecretJWTKeyForSigningThatImGOnnaPutInEnvLAter',
+            defaultOptions
+        )
+    }
+
+}
---
+++ apps/web/src/core/token-store.ts
@@ -0,0 +1,43 @@
+import {assertNever} from "./typeguards";
+import is from "@sindresorhus/is";
+
+const localStorageKey = '__bluewind_token__';
+let localStorageTokenStore: LocalStorageTokenStore;
+
+type TokenStoreStrategy = 'local_storage';
+
+interface ITokenStore {
+    setToken(token: string): void;
+    removeToken(): void;
+    getToken(): string | null;
+}
+
+class LocalStorageTokenStore implements ITokenStore {
+
+    setToken(token: string): void {
+        window.localStorage.setItem(localStorageKey, token);
+    }
+
+    removeToken(): void {
+        window.localStorage.removeItem(localStorageKey);
+    }
+
+    getToken(): string | null {
+        return window.localStorage.getItem(localStorageKey);
+    }
+}
+
+
+
+export function getTokenStore(type: TokenStoreStrategy="local_storage"): ITokenStore {
+    switch (type) {
+        case 'local_storage':
+            if (!localStorageTokenStore) {
+                localStorageTokenStore = new LocalStorageTokenStore();
+            }
+            return localStorageTokenStore;
+        default:
+            assertNever(type);
+            throw new Error(`Unsupported token store strategy '${type}'`);
+    }
+}
