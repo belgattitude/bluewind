4a6ea554219a9d13d4c581827a1bcff70072b034
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-09-23 12:29:55 +0200

    Added temp typeguards

---
+++ apps/web/src/core/typeguards.ts
@@ -0,0 +1,13 @@
+// typeguard
+import is from "@sindresorhus/is";
+import {ApiResponse} from "./api.interfaces";
+
+export function assertNever(value: never): never {
+    throw new Error(`Unexpected value: ${value}`);
+}
+
+export function isApiResponse(response: unknown): response is ApiResponse {
+    console.log('response', response);
+    console.log('response', is.plainObject(response) && response.success !== undefined && response.data !== undefined);
+    return is.plainObject(response) && response.success !== undefined && response.data !== undefined;
+}
