909b311164400d6b9014b4231ba9602a5a9ef952
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-09-30 11:54:48 +0200

    Make lint-staged actually works

--- packages/error-flow/example/index.ts
+++ packages/error-flow/example/index.ts
@@ -1,15 +1,13 @@
-import {Result} from "../src";
+import { Result } from '../src';
 
 type UserData = {
     username: string;
-}
+};
 
-const {payload } = Result.ok<UserData>({username: 'John'});
+const { payload } = Result.ok<UserData>({ username: 'John' });
 
 if (payload.isError) {
     throw payload.error;
 }
 
 console.log(payload.value.username);
-
-type Test = { user: string, user2: string, user3: string, user4: string, user5: string, user6: string,user7: string, user8: string,   }
