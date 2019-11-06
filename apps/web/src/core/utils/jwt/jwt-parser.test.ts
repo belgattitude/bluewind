6a1cc3cd09d04d34fa38b3686ce03b0028cbe9c2
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-11-06 22:37:03 +0100

    fix: fix invalid assertion

--- apps/web/src/core/utils/jwt/jwt-parser.test.ts
+++ apps/web/src/core/utils/jwt/jwt-parser.test.ts
@@ -5,7 +5,7 @@ describe('JWTParser tests', () => {
         const token =
             'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
         const payload = JwtParser.getPayload(token);
-        expect(payload).toBe({
+        expect(payload).toStrictEqual({
             sub: '1234567890',
             name: 'John Doe',
             iat: 1516239022,
