fe3b76b6c100801f3af0c40dc6a7b865900f725b
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-11-06 22:36:33 +0100

    feat: Add custom router history

--- apps/web/src/authenticated-app.tsx
+++ apps/web/src/authenticated-app.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import { PrivateRoute } from './component/route/private-route';
 import { ProfilePage } from './features/profile/profile.page';
 import { HomePage } from './features/home/home.page';
 import { StudentPage } from './features/student/student.page';
 import { Layout } from './component/layout/layout';
+import { browserHistory } from './component/router/history';
 
 const AuthenticatedApp: React.FC = () => {
     return (
         <div className="app">
-            <Router>
+            <Router history={browserHistory}>
                 <Layout>
                     <Switch>
                         <Route path="/" exact component={HomePage} />
