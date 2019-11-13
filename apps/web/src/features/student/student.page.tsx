6ac9c28da2b3ff2295d96b4745fb0d45562d3dd6
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-11-13 22:05:38 +0100

    fix: add error message on fech failure

--- apps/web/src/features/student/student.page.tsx
+++ apps/web/src/features/student/student.page.tsx
@@ -20,9 +20,11 @@ const SearchBox: React.FC = props => {
     const search = useStudentSearch();
     const timeout = 150;
     const searchRef = useRef<HTMLInputElement>(null);
+
     const [debouncedCallback] = useDebouncedCallback(query => {
         search.search(query);
     }, timeout);
+
     useEffect(() => {
         if (searchRef && searchRef.current) {
             searchRef.current.focus();
@@ -49,10 +51,14 @@ const SearchBox: React.FC = props => {
 const List: React.FC<{
     handleSelected: (studentId: number) => void;
 }> = props => {
-    const search = useStudentSearch();
+    const { loading, data, error } = useStudentSearch();
     return (
         <div className="test-list">
-            <StudentList loading={search.loading} students={search.data} handleSelected={props.handleSelected} />
+            {error ? (
+                <div>{error}</div>
+            ) : (
+                <StudentList loading={loading} students={data} handleSelected={props.handleSelected} />
+            )}
         </div>
     );
 };
