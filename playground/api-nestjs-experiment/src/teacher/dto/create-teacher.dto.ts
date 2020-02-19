e93c21e55aa17304aff97f2248a83d2f43c42ef1
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-02-19 10:53:29 +0100

    feat: updated examples to nextjs swagger 4

--- playground/api-nestjs-experiment/src/teacher/dto/create-teacher.dto.ts
+++ playground/api-nestjs-experiment/src/teacher/dto/create-teacher.dto.ts
@@ -1,28 +1,9 @@
-import { ApiModelProperty } from '@nestjs/swagger';
 import { IsEmail } from 'class-validator';
 
 export class CreateTeacherDto {
-    @ApiModelProperty({
-        description: 'Last name',
-        required: true,
-        maxLength: 80,
-        minLength: 2,
-    })
     @IsEmail()
     readonly lastName!: string;
 
-    @ApiModelProperty({
-        description: 'First name',
-        required: true,
-        maxLength: 80,
-        minLength: 2,
-    })
     readonly firstName!: string;
-    @ApiModelProperty({
-        description: 'Email',
-        required: true,
-        maxLength: 80,
-        minLength: 5,
-    })
     readonly email!: string;
 }
